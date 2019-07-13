85089ea62b8fbb90e18b5c59cf722f301e33fb1c
Patrick Scott <patrick@cognitoforms.com>
2019-07-12 20:07:50 -0400

    Add poloniex

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const bot = require("./bot")
-console.log(bot);
+const Poloniex = require('poloniex.js')
+const poloniex = new Poloniex(process.env.API_KEY, process.env.API_SECRET)
+require('dotenv').config();
+
+console.log(poloniex);
