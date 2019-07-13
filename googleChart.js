9a4a39e02c12560e6aa1223b98cf175d7b9596eb
Patrick Scott <patrick@cognitoforms.com>
2019-07-13 01:03:01 -0400

    Fixing chart direction

--- googleChart.js
+++ googleChart.js
@@ -1,4 +1,4 @@
 module.exports = {
   head: `<html><head><script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script><script type="text/javascript">google.charts.load('current', {'packages':['corechart']});google.charts.setOnLoadCallback(drawChart);function drawChart() {var data = new google.visualization.DataTable();data.addColumn('string', 'time');data.addColumn('number', 'value');data.addColumn({type: 'string', role:'annotation'});data.addColumn({type: 'string', role:'annotationText'});data.addColumn('number', 'trend');data.addRows([`,
-  tail:`]);var options = {title: 'Price Chart',legend: { position: 'bottom' }};var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));chart.draw(data, options);}</script></head><body><div id="curve_chart" style="width: 100%; height: 100%"></div></body></html>`
+  tail:`]);var options = {title: 'Price Chart',legend: { position: 'bottom' }, hAxis: { direction: '-1'}};var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));chart.draw(data, options);}</script></head><body><div id="curve_chart" style="width: 100%; height: 100%"></div></body></html>`
 }
