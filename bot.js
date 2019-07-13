cdae4af7473c356795ce2bd058926e21aabbf8da
Patrick Scott <patrick@cognitoforms.com>
2019-07-12 23:56:01 -0400

    Added google chart utility

--- bot.js
+++ bot.js
@@ -1,22 +1,26 @@
 const yargs = require("yargs").alias({
   'i': 'interval',
   'c': 'currency',
-  'p': 'points'
+  'p': 'points',
+  's': 'start',
+  'e': 'end'
 })
 const argv = yargs.argv
 const validIntervals = [300,900,1800,7200,14400,86400]
 
 function Bot()
 {
-  const { interval, currency, points } = argv
-  this.interval = interval || 10 // [300,900,1800,7200,14400,86400]
-  this.currency = currency || "BTC_XMR"
+  const { interval, currency, points, start, end } = argv
+  this.interval = interval || 300 // Poloniex-allowed candlestick periods [300,900,1800,7200,14400,86400]
+  this.currency = currency || "BTC_XMR" // Default currency pair
+  this.majorCurrency = this.currency.split("_")[0] // BTC
+  this.minorCurrency = this.currency.split("_")[1] // XMR
   this.dataPoints = points || []
+  this.startTime = start || 1491048000 // April 17, 7 A.M
+  this.endTime = end || this.startTime + 3600000 // 12 hours from start time
   this.prices = []
   this.currentMovingAverage = 0
   this.lengthOfMA = 0
-  this.startTime = false
-  this.endTime = false
   this.historicalData = false
   this.tradePlaced = false
   this.typeOfTrade = false
--- index.js
+++ index.js
@@ -1,6 +1,70 @@
+const fs = require('fs');
 const bot = require("./bot")
+const gChart = require('./googleChart')
 const Poloniex = require('poloniex.js')
 const poloniex = new Poloniex(process.env.API_KEY, process.env.API_SECRET)
 require('dotenv').config();
 
-console.log(poloniex);
+console.log("Fetching chart data");
+fetchChartData(bot)
+
+function fetchChartData(bot)
+{
+  poloniex.returnChartData(bot.majorCurrency, bot.minorCurrency, bot.interval, bot.startTime, bot.endTime)
+  .then(points => {
+    bot.historicalData = points
+    generateHistoricalDataChart(bot.historicalData)
+  })
+}
+
+function generateHistoricalDataChart(historicalData)
+{
+  console.log('Generating Historical Data Chart');
+  const chartHtml = buildHtmlFile(historicalData)
+
+  fs.writeFile('chart.html', chartHtml, function(err){
+    if(err) throw new Error(err)
+    console.log('Chart Saved');
+  })
+}
+
+function buildHtmlFile(historicalData)
+{
+  console.log('Building Html File');
+
+  let nextDataPoint
+  let lastPairPrice
+  let dataDate
+  let dataPoints = []
+  let pointString = ``
+  let done = true
+
+  while(done)
+  {
+    if(historicalData.length > 0)
+    {
+        nextDataPoint = historicalData.pop()
+        lastPairPrice = nextDataPoint.weightedAverage
+        dataDate = new Date(nextDataPoint.date)
+    }
+    else
+    {
+      dataPoints.forEach(point => {
+        pointString += `['${point['date']}',${point['price']},${point['label']},${point['description']},${point['trend']}`
+        pointString += "],\n"
+      })
+      done = false
+      break;
+    }
+
+    dataPoints.push({
+      date: dataDate,
+      price: lastPairPrice,
+      trend: bot.currentResistance,
+      label: 'null',
+      description: 'null'
+    })
+  }
+
+  return gChart.head + pointString + gChart.tail
+}
