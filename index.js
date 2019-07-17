22aafd307e448057e76a12cbd4db2c727c5e4758
Patrick Scott <patrick@cognitoforms.com>
2019-07-16 23:10:11 -0400

    Added way to store credentials

--- index.js
+++ index.js
@@ -2,18 +2,44 @@
 
 const PoloniexBot = require("./Bot")
 const bot = new PoloniexBot()
+const fs = require('fs')
 const BotCandlestick = require('./resources/BotCandlestick')
 const colors = require('colors')
+const prompt = require('prompt');
+const yargs = require("yargs").alias({
+  'config':'config'
+})
+const argv = yargs.argv
+require('dotenv').config();
+prompt.message = "Enter your";
 
-if(bot.live)
+if(argv.config || !process.env.API_KEY && !process.env.API_SECRET)
 {
-  console.log(`💰 Live Trading Mode... (Major: ${bot.majorCurrency}) (Minor: ${bot.minorCurrency}) (Interval: ${bot.interval}s) (Real Money: ${bot.realMoney}) (Trade Amount: ${bot.tradeAmount}) (Stop Loss: ${bot.stopLoss})`.yellow);
-  liveTrader()
+  prompt.get(['API Key', 'Secret'], function (err, result) {
+    const credentials = "API_KEY=" + result['API Key'] + "\nAPI_SECRET="+ result['Secret']
+
+    // Save credentials to .env file
+    fs.writeFile(`.env`, credentials, function(err){
+      if(err) throw new Error(err)
+      console.log('Credentials stored');
+    })
+  });
 }
 else
+  init()
+
+function init()
 {
-  console.log(`📊 Backtesting Mode... (Major: ${bot.majorCurrency}) (Minor: ${bot.minorCurrency}) Period: ${bot.period}s Start: ${bot.startTime} End: ${bot.endTime}`.magenta);
-  backTest()
+  if(bot.live)
+  {
+    console.log(`💰 Live Trading Mode... (Major: ${bot.majorCurrency}) (Minor: ${bot.minorCurrency}) (Interval: ${bot.interval}s) (Real Money: ${bot.realMoney}) (Trade Amount: ${bot.tradeAmount}) (Stop Loss: ${bot.stopLoss})`.yellow);
+    liveTrader()
+  }
+  else
+  {
+    console.log(`📊 Backtesting Mode... (Major: ${bot.majorCurrency}) (Minor: ${bot.minorCurrency}) Period: ${bot.period}s Start: ${bot.startTime} End: ${bot.endTime}`.magenta);
+    backTest()
+  }
 }
 
 async function liveTrader()
